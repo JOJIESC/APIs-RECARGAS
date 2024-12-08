3a3da17a383f55d25991c2e3352845026c6a5003
jorgescx <j_escobart@hotmail.com>
2024-12-08 17:26:22 -0600

    cors fix

--- api-minisuper/index.js
+++ api-minisuper/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { postRecarga } from "./recargasRoute.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors(
+  {
+    origin: "http://localhost:3000"
+  }
+));
 app.use(express.json());
 app.post("/recargas", postRecarga);
 
